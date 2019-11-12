7937603976db8da139ddab8c3e1a7ad74ccef539
Carles Serna <carles.serna@gmail.com>
2019-11-12 21:44:25 +0100

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,41 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Hi! I'm <a href="https://www.instagram.com/albfs/ " target="_blank">Alba</a>, an artist based in <a href="https://www.instagram.com/wearecontracollective/" target="_blank">Barcelona</a></h1>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
+  name: 'app'
 }
 </script>
 
 <style>
+body {
+  background-image:url(./assets/alba.jpg);
+  background-position: center center;
+  background-repeat: no-repeat;
+}
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: white;
+}
+
+h1 {
+    left: 25%;
+    line-height: 100px;
+    margin-top: -100px;
+    position: absolute;
+    text-align: center;
+    top: 45%;
+    width: 50%;
+    font-size: 4rem !important;
+}
+
+a {
+  color: #42b983;
 }
 </style>
